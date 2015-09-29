a5cfed16120d74b5228db5dd29e12e5a4f547928
phatsk <phatsk@gmail.com>
2015-09-28 23:48:27 -0400

    Minor JS tweak for ready event

--- js/pebble-js-app.js
+++ js/pebble-js-app.js
@@ -1,3 +1,9 @@
-Pebble.addEventListener('showConfiguration', function(e) {
-	Pebble.openURL('destiny-phatsk.rhcloud.com/jade-rabbit.php');
+Pebble.addEventListener('ready', function() {
+	initHooks();
 });
+
+function initHooks() {
+	Pebble.addEventListener('showConfiguration', function(e) {
+		Pebble.openURL('destiny-phatsk.rhcloud.com/jade-rabbit.php');
+	});
+}
