85673cc14d2e9c8b2ce7475f396501acd5b0f112
phatsk <phatsk@gmail.com>
2015-09-29 00:10:54 -0400

    Config updates

--- src/pebble-js-app.js
+++ src/pebble-js-app.js
@@ -6,4 +6,8 @@ function initHooks() {
 	Pebble.addEventListener('showConfiguration', function(e) {
 		Pebble.openURL('destiny-phatsk.rhcloud.com/jade-rabbit.php');
 	});
+
+	Pebble.addEventListener('webviewclosed', function(e) {
+		console.log('Configuration window returned: ' + e.response);
+	});
 }
